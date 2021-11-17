aa93325ad7642d2696746a5b2cee4b2abf415959
CortesWD <cccp.designer@gmail.com>
2021-11-16 19:24:12 -0500

    added sinon to mock fns

--- rest-api-e2e/test/is-auth-mw.js
+++ rest-api-e2e/test/is-auth-mw.js
@@ -1,7 +1,55 @@
 const { expect } = require('chai');
+const sinon = require('sinon');
+const jwt = require('jsonwebtoken');
 
-const isAuth = require('./../middleware/is-auth');
+const isAuthMw = require('./../middleware/is-auth');
 
-it('should throw error if not auth', () => {
-  
+describe('middleware/is-auth', () => {
+
+  it('should throw error if not auth:', () => {
+    const req = {
+      get: (headerName) => null
+    };
+
+    expect(isAuthMw.bind(this, req, {}, () => { }))
+      .to.throw('not authenticated.');
+
+  });
+
+  it('should throw error if auth header is only 1 string', () => {
+    const req = {
+      get: (headerName) => 'xyz'
+    };
+
+    expect(isAuthMw.bind(this, req, {}, () => { }))
+      .to.throw();
+  });
+
+  it('should throw error if token cannot be verified', () => {
+    const req = {
+      get: (headerName) => 'Bearer xyz'
+    };
+
+    expect(isAuthMw.bind(this, req, {}, () => { }))
+      .to.throw();
+  });
+
+  it('should yield a userId after decoding token', () => {
+    const req = {
+      get: (headerName) => 'Bearer asdfasdfasdf'
+    };
+
+    sinon.stub(jwt, 'verify');
+
+    jwt.verify.returns({ userId: 'abc' });
+
+    isAuthMw(req, {}, () => { });
+
+    expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
+
+    jwt.verify.restore();
+  });
+
 });
