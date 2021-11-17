cd11f889f15b600427c4486a5ea8756d3e99be6c
CortesWD <cccp.designer@gmail.com>
2021-11-16 21:33:54 -0500

    using promises

--- es-modules/response-handler.js
+++ es-modules/response-handler.js
@@ -1,3 +1,4 @@
+import fs from 'fs/promises';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -5,7 +6,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
+  fs.readFile('my-page.html', 'utf8')
+    .then((data) => {
+      res.send(data);
+    })
+    .catch(err => console.log(err))
+
+
   //__dirname and other global vars are disabled with es modules
   // needs to be defined manually
-  res.sendFile(path.join(__dirname, 'my-page.html'));
+  // res.sendFile(path.join(__dirname, 'my-page.html'));
 };
