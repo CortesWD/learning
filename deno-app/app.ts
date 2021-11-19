6b683cf8e5cdfc5b7d296c8a7c3e2333971c6004
CortesWD <cccp.designer@gmail.com>
2021-11-18 19:18:34 -0500

    deno init

---
+++ deno-app/app.ts
@@ -0,0 +1,15 @@
+const text = 'hey nea';
+
+const encoder = new TextEncoder();
+
+const data = encoder.encode(text);
+
+//Deno needs correct permissions to execute tasks
+// to write should be --allow-write
+// built-in features: https://doc.deno.land/builtin/stable
+// permissions https://deno.land/manual@v1.16.2/getting_started/permissions
+
+Deno.writeFile('./message.txt', data)
+  .then(() => {
+    console.warn('file created')
+  });
